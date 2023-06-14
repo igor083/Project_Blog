56b91e707e8210cd8bed58ba4f4b1f3c5a7799b8
Igor <igor8244@gmail.com>
2023-06-14 14:39:23 -0300

    Interacao com banco de dados, mostrando categorias na rota

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
-
+const Category = require('./Category');
+const slugify = require('slugify');//transforma a string e um formato para a url (titulo aleatorio => titulo-aleatorio)
 
 router.get('/admin/categories/new',  (req, res) =>{
     res.render("admin/categories/new");
+});
+router.post('/categories/save',(req,res)=>{
+    var title = req.body.title;
+    if(title!= undefined){
+
+        Category.create({
+            title:title,
+            slug:slugify(title)
+        }).then(()=>{
+            res.redirect('/')
+        })
+
+    }else{
+        res.redirect('/admin/categories/new');
+    }
+
+    
+});
+router.get("/admin/categories", (req,res)=>{
+Category.findAll().then(categories=>{
+    res.render("admin/categories/CategoriesIndex",{categories:categories})
+
+})
+
 })
 
 module.exports = router;
