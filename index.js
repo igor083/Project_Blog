cd479e8dda7f58eb2b9f1c3e185ffc2c58f6c05d
Igor <igor8244@gmail.com>
2023-07-12 11:00:23 -0300

    Home page criada, article page criada

--- index.js
+++ index.js
@@ -34,8 +34,29 @@ app.use("/", categoriesController);//usando as rotas que estao dentro do arquivo
 app.use("/", articlesController);//usando as rotas que estao dentro do arquivo articles controller
 
 app.get("/", (req, res) => {
-    res.render('index');
-});
+    Article.findAll().then(articles => {
+        res.render('index',{articles:articles});
+
+    })
+})
+
+app.get("/:slug",(req,res)=>{
+    var slug = req.params.slug;
+    Article.findOne({
+        where:{
+            slug:slug
+        }
+    }).then(article=>{
+        if(article != undefined){
+            res.render("article",{article:article});
+        }else res.redirect("/");
+
+    }).catch(erro =>{
+        res.redirect("/");
+    })
+
+
+})
 
 app.listen(8080, () => {
     console.log("Servidor rodando");
